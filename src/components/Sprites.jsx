e9c1fe2a2df40ee4be4521a67da334bcd857d717
Nico Gambolati <nicolasgambolati@gmail.com>
2024-03-27 21:34:08 -0300

    fixes somes styles

--- src/components/Sprites.jsx
+++ src/components/Sprites.jsx
@@ -2,7 +2,7 @@
 
 export const Sprites = ({sprites}) => {
   return (
-    <div>
+    <div className="d-flex justify-content-center">
     {sprites.map((sprite) => (
       <img key={sprite} src={sprite} alt={sprite} width={250} height={250}/>
     ))}
